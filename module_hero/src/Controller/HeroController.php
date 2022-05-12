567c6fca03b492fe6658ddcf8ab0f8ef6f3cde37
VishalESD <48333506+VishalESD@users.noreply.github.com>
2022-05-12 16:55:16 -0400

    Create HeroController.php

---
+++ module_hero/src/Controller/HeroController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\module_hero\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ *
+ * This is our hero controller
+ */
+class HeroController{
+  public function heroList(){
+    $heroes = [
+      ['name' => 'Hulk'],
+      ['name' => 'Super Man'],
+      ['name' => 'Iron Man'],
+      ['name' => 'Dare Devil'],
+      ['name' => 'Wolverine'],
+    ];
+
+    $ourHeroes = '';
+
+    foreach ($heroes as $hero){
+      $ourHeroes .= '<li>' . $hero['name'] . '</li>';
+    }
+
+    return [
+      '#type' => 'markup',
+      '#markup' => '<ol>' . $ourHeroes . '</ol>',
+    ];
+  }
+}
