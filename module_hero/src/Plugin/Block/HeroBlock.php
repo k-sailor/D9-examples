d7dc1d7e2e30b93ffc127ef7832d2ec4c47998b1
VishalESD <48333506+VishalESD@users.noreply.github.com>
2022-05-17 13:56:17 -0400

    Update HeroBlock.php

--- module_hero/src/Plugin/Block/HeroBlock.php
+++ module_hero/src/Plugin/Block/HeroBlock.php
@@ -21,9 +21,37 @@ class HeroBlock extends BlockBase
    */
   public function build()
   {
-    return [
-      '#type' => 'markup',
-      '#markup' => $this->t('The output of our superheroes block.'),
+
+    $heroes = [
+      ['name' => 'Hulk', 'real_name' => 'David Banner'],
+      ['name' => 'Thor', 'real_name' => 'Thor Odinson'],
+      ['name' => 'Iron Man', 'real_name' => 'Tony Stark'],
+      ['name' => 'Luke Cage', 'real_name' => 'Carl Lucas'],
+      ['name' => 'Black Widow', 'real_name' => 'Natalia Romanova'],
+      ['name' => 'Dare Devil', 'real_name' => 'Matthew Murdock'],
+      ['name' => 'Captain America', 'real_name' => 'Steven Rogers'],
+      ['name' => 'Wolverine', 'real_name' => 'James Howlett'],
     ];
+
+    $table = [
+      '#type' => 'table',
+      '#header' => [
+        $this->t('Name'),
+        $this->t('Real name'),
+      ]
+    ];
+
+    foreach ($heroes as $hero) {
+      $table[] = [
+        'hero_name' => [
+          '#type' => 'markup',
+          '#markup' => $hero['name'],
+        ],
+        'real_name' => [
+          '#type' => 'markup',
+          '#markup' => $hero['real_name'],
+        ],
+      ];
+    }
   }
 }
