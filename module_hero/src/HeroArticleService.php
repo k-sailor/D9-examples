8ee109fa0bc99f1b311bdeeb92126caba315d044
VishalESD <48333506+VishalESD@users.noreply.github.com>
2022-05-17 20:52:53 -0400

    Update HeroArticleService.php

--- module_hero/src/HeroArticleService.php
+++ module_hero/src/HeroArticleService.php
@@ -2,17 +2,38 @@
 
 namespace Drupal\module_hero;
 
+//use Drupal\Core\Entity\Query\Null\QueryFactory
+// use Drupal\Core\Config\Entity\Query\QueryFactory;
+// use Drupal\Core\Entity\Query\QueryFactory;
+use Drupal\Core\Config\Entity\Query\QueryFactory;
+// use Drupal\Core\Entity\Query\QueryInterface;
+use Drupal\Core\Entity\EntityTypeManager;
+
 /**
  * Our hero article service class.
  */
 class HeroArticleService {
   
+  private $entityQuery;
+  private $entityManager;
+
+  public function __construct(QueryFactory $entityQuery, EntityTypeManager $entityManager){
+    $this->$entityQuery = $entityQuery;
+    $this->$entityManager = $entityManager;
+  }
+
   /**
    * Method for getting articles regarding heroes.
    */
   public function getHeroArticles(){
     $articles = ['Hulk is green!', 'Flash is red!'];
+
+    kint($this->entityQuery->get('node')->condition('type', 'article')->execute());
+    kint($entityManager);
     return $articles;
   }
 
 }
+
+// $hs = Drupal::service('module_hero.hero_articles');
+// kint($hs->getHeroArticles()); die();
